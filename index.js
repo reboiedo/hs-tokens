37f9d04cb070a040ce5deb15a3f245bb3864b379
reboiedo <javier@rebolledo.mx>
2026-03-07 13:09:07 -0600

    Simplify package to only publish tokens folder

--- index.js
+++
@@ -1,4 +0,0 @@
-const tokens = require('./tokens/tokens.json');
-const metadata = require('./tokens/$metadata.json');
-
-module.exports = { tokens, metadata };
--- index.mjs
+++
@@ -1,5 +0,0 @@
-import tokens from './tokens/tokens.json' with { type: 'json' };
-import metadata from './tokens/$metadata.json' with { type: 'json' };
-
-export { tokens, metadata };
-export default tokens;
